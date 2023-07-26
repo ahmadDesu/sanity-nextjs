70e0f94cc92948eb26571812e5321837b227730d
Ahmad Adyla <madrista982@gmail.com>
2023-07-26 18:25:35 +0700

    memperbaiki link src image

--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,7 @@ export default function IndexPage({ pets }) {
 
 function getImageSrc(imageRef) {
   const imageName = imageRef.replace("image-", "").replace(/-/g, ".");
-  return imageName;
+  return `https://cdn.sanity.io/images/mih1agps/production/${imageName}`;
 }
 
 function renderPetContent(content) {
