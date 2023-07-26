b1f17f3fd5c8f754eaf7789b05276d3c799d7747
Ahmad Adyla <madrista982@gmail.com>
2023-07-26 16:03:27 +0700

    initialized api

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import { createClient } from "next-sanity";
 export default function IndexPage({ pets }) {
   return (
     <>
@@ -5,7 +6,7 @@ export default function IndexPage({ pets }) {
         <h1>Sanity + Next.js</h1>
       </header>
       <main>
-        <h2>Pets</h2>
+        <h2>pets</h2>
         {pets.length > 0 && (
           <ul>
             {pets.map((pet) => (
@@ -33,17 +34,15 @@ export default function IndexPage({ pets }) {
   );
 }
 
+const client = createClient({
+  projectId: "mih1agps",
+  dataset: "production",
+  apiVersion: "2021-10-21",
+  useCdn: false
+});
+
 export async function getStaticProps() {
-  const pets = [
-    /* {
-      _createdAt: "2022-03-08T09:28:00Z",
-      _id: "1f69c53d-418a-452f-849a-e92466bb9c75",
-      _rev: "xnBg0xhUDzo561jnWODd5e",
-      _type: "pet",
-      _updatedAt: "2022-03-08T09:28:00Z",
-      name: "Bamse"
-    } */
-  ];
+  const pets = await client.fetch(`*[_type == "pet"]`);
 
   return {
     props: {
